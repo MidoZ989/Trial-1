23d40da9ba303db3be74cb0ac65f8db5a29222de
Mido <midotmam989@gmail.com>
2021-05-26 23:45:04 +0200

    VideoCall

---
+++ android/app/src/main/kotlin/com/example/agoratrial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agoratrial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = "ddad091a454346f6a11895102bbc988c";
+const Token = "Your_Token";
